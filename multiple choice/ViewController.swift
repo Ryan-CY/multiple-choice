94f87a8f10f79b41b5c5ca7684b33d05d58fbc74
Ryan CY <phillin820109@msn.com>
2022-09-23 10:29:28 +0800

    Initial Commit

---
+++ multiple choice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multiple choice
+//
+//  Created by Ryan Lin on 2022/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
